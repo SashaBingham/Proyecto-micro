ffb4c1b4bec49aadb05670f2b94094aedaa4f20f
SashaBingham <119087012+SashaBingham@users.noreply.github.com>
2022-11-25 23:55:01 -0600

    Update lib_original.h

--- lib_original.h
+++ lib_original.h
@@ -1,133 +1,218 @@
+#ifndef TEMPHUM11_H
+#define TEMPHUM11_H
 
-#include "temphum11.h"
+#include "drv_digital_out.h"
+#include "drv_digital_in.h"
+#include "drv_i2c_master.h"
 
+// -------------------------------------------------------------- PUBLIC MACROS 
+/**
+ * \defgroup macros Macros
+ * \{
+ */
 
-// ---------------------------------------------- PRIVATE FUNCTION DECLARATIONS 
+/**
+ * \defgroup map_mikrobus MikroBUS
+ * \{
+ */
+#define TEMPHUM11_MAP_MIKROBUS( cfg, mikrobus ) \
+  cfg.scl  = MIKROBUS( mikrobus, MIKROBUS_SCL ); \
+  cfg.sda  = MIKROBUS( mikrobus, MIKROBUS_SDA )
+/** \} */
 
-static void com_delay ( );
-static void conv_delay ( );
+/**
+ * \defgroup error_code Error Code
+ * \{
+ */
+#define TEMPHUM11_RETVAL  uint8_t
 
-// ------------------------------------------------ PUBLIC FUNCTION DEFINITIONS
+#define TEMPHUM11_OK           0x00
+#define TEMPHUM11_INIT_ERROR   0xFF
+/** \} */
 
-void temphum11_cfg_setup ( temphum11_cfg_t *cfg )
+/**
+ * \defgroup registers Registers
+ * \{
+ */
+#define TEMPHUM11_REG_TEMPERATURE               0x00
+#define TEMPHUM11_REG_HUMIDITY                  0x01
+#define TEMPHUM11_REG_CONFIGURATION             0x02
+#define TEMPHUM11_REG_SERIAL_ID_0               0xFB
+#define TEMPHUM11_REG_SERIAL_ID_1               0xFC
+#define TEMPHUM11_REG_SERIAL_ID_2               0xFD
+#define TEMPHUM11_REG_MANUFACTURER_ID           0xFE
+#define TEMPHUM11_REG_DEVICE_ID                 0xFF
+/** \} */
+
+/**
+ * \defgroup configuration Configuration
+ * \{
+ */
+#define TEMPHUM11_NORMAL_OPERATION              0x0000
+#define TEMPHUM11_SOFTWARE_RESET                0x8000
+#define TEMPHUM11_HEATER_DISABLED               0x0000
+#define TEMPHUM11_HEATER_ENABLED                0x2000
+#define TEMPHUM11_TEMP_HUM_ACQUIRED             0x0000
+#define TEMPHUM11_TEMP_FIRST                    0x1000
+#define TEMPHUM11_BAT_VOLT_BIGGER_THAN_2p8      0x0000
+#define TEMPHUM11_BAT_VOLT_SMALLER_THAN_2p8     0x0800
+#define TEMPHUM11_TEMP_RESOLUTION_14bit         0x0000
+#define TEMPHUM11_TEMP_RESOLUTION_11bit         0x0400
+#define TEMPHUM11_HUM_RESOLUTION_14bit          0x0000
+#define TEMPHUM11_HUM_RESOLUTION_11bit          0x0100
+#define TEMPHUM11_HUM_RESOLUTION_8bit           0x0200
+/** \} */
+
+/**
+ * \defgroup slave_addr Slave Address
+ * \{
+ */
+#define TEMPHUM11_DEVICE_SLAVE_ADDR             0x40
+/** \} */
+
+/**
+ * \defgroup  return_value  Return Value
+ * \{
+ */
+#define TEMPHUM11_DEVICE_ID                     0x1050
+#define TEMPHUM11_MANUFACTURER_ID               0x5449
+/** \} */
+
+/**
+ * \defgroup temperature_data Temperature data in ..
+ * \{
+ */
+#define TEMPHUM11_TEMP_IN_CELSIUS               0x00
+#define TEMPHUM11_TEMP_IN_KELVIN                0x01
+#define TEMPHUM11_TEMP_IN_FAHRENHEIT            0x02
+/** \} */
+/** \} */ // End group macro 
+// --------------------------------------------------------------- PUBLIC TYPES
+/**
+ * \defgroup type Types
+ * \{
+ */
+
+/**
+ * @brief Click ctx object definition.
+ */
+typedef struct
+{
+     // Modules 
+
+    i2c_master_t i2c;
+
+    // ctx variable 
+
+    uint8_t slave_address;
+
+} temphum11_t;
+
+/**
+ * @brief Click configuration structure definition.
+ */
+typedef struct
 {
     // Communication gpio pins 
 
-    cfg->scl = HAL_PIN_NC;
-    cfg->sda = HAL_PIN_NC;
+    pin_name_t scl;
+    pin_name_t sda;
 
+    // Static variable 
 
-    cfg->i2c_speed = I2C_MASTER_SPEED_STANDARD; 
-    cfg->i2c_address = TEMPHUM11_DEVICE_SLAVE_ADDR;
+    uint32_t i2c_speed;
+    uint8_t i2c_address;
+
+} temphum11_cfg_t;
+
+/** \} */ // End types group
+// ----------------------------------------------- PUBLIC FUNCTION DECLARATIONS
+
+/**
+ * \defgroup public_function Public function
+ * \{
+ */
+ 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/**
+ * @brief Config Object Initialization function.
+ *
+ * @param cfg  Click configuration structure.
+ *
+ * @description This function initializes click configuration structure to init state.
+ * @note All used pins will be set to unconnected state.
+ */
+void temphum11_cfg_setup ( temphum11_cfg_t *cfg );
+
+/**
+ * @brief Initialization function.
+ * @param temphum11 Click object.
+ * @param cfg Click configuration structure.
+ * 
+ * @description This function initializes all necessary pins and peripherals used for this click.
+ */
+TEMPHUM11_RETVAL temphum11_init ( temphum11_t *ctx, temphum11_cfg_t *cfg );
+
+/**
+ * @brief Click Default Configuration function.
+ *
+ * @param ctx  Click object.
+ *
+ * @description This function executes default configuration for TempHum11 click.
+ */
+void temphum11_default_cfg ( temphum11_t *ctx );
+
+/**
+ * @brief Function for writing configuration device for measurement.
+ *
+ * @param ctx          Click object.
+ * @param config       Data to be written.
+ *
+ * @description This function writes configuration device for measurement.
+ */
+void temphum11_write_config ( temphum11_t *ctx, uint16_t config );
+
+/**
+ * @brief Functions for reading data from registers
+ *
+ * @param ctx          Click object.
+ * @param reg          Register address.
+ * 
+ * @returns 16-bit data that is reads from the register.
+ *
+ * @description This function reads data from the desired register.
+ */
+uint16_t temphum11_read_data ( temphum11_t *ctx, uint8_t reg );
+
+/**
+ * @brief Functions for reading Temperature data
+ *
+ * @param ctx          Click object.
+ * @param temp_in      Macro for determinating temperature unit ... (FAHRENHEIT, KELVIN, CELSIUS)
+ * 
+ * @returns temperature data
+ * 
+ * @description This function reads temperature data in desired unit.
+ */
+float temphum11_get_temperature ( temphum11_t *ctx, uint8_t temp_in );
+
+/**
+ * @brief Functions for reading Relative Huminidy data
+ *
+ * @param ctx          Click object.
+ * 
+ * @returns Relative Huminidy data in [%RH]
+ * 
+ * description This function reads relative huminidy data
+ */
+float temphum11_get_humidity ( temphum11_t *ctx );
+
+#ifdef __cplusplus
 }
-
-TEMPHUM11_RETVAL temphum11_init ( temphum11_t *ctx, temphum11_cfg_t *cfg )
-{
-     i2c_master_config_t i2c_cfg;
-
-    i2c_master_configure_default( &i2c_cfg );
-    i2c_cfg.speed  = cfg->i2c_speed;
-    i2c_cfg.scl    = cfg->scl;
-    i2c_cfg.sda    = cfg->sda;
-
-    ctx->slave_address = cfg->i2c_address;
-
-    if ( i2c_master_open( &ctx->i2c, &i2c_cfg ) == I2C_MASTER_ERROR )
-    {
-        return TEMPHUM11_INIT_ERROR;
-    }
-
-    i2c_master_set_slave_address( &ctx->i2c, ctx->slave_address );
-    i2c_master_set_speed( &ctx->i2c, cfg->i2c_speed );
-
-    return TEMPHUM11_OK;
-}
-
-void temphum11_default_cfg ( temphum11_t *ctx )
-{
-    temphum11_write_config( ctx, TEMPHUM11_NORMAL_OPERATION | TEMPHUM11_HEATER_DISABLED |
-                                 TEMPHUM11_TEMP_HUM_ACQUIRED | TEMPHUM11_TEMP_RESOLUTION_14bit |
-                                 TEMPHUM11_HUM_RESOLUTION_14bit ); 
-}
-
-void temphum11_write_config ( temphum11_t *ctx, uint16_t config )
-{
-    uint8_t write_reg[ 3 ];
-    
-    write_reg[ 0 ] = TEMPHUM11_REG_CONFIGURATION;
-    write_reg[ 1 ] = ( uint8_t )( config >> 8 );
-    write_reg[ 2 ] = ( uint8_t )( config );  
-
-    i2c_master_write( &ctx->i2c, write_reg, 3 );  
-}
-
-uint16_t temphum11_read_data ( temphum11_t *ctx, uint8_t reg )
-{
-    uint8_t write_reg[ 1 ];
-    uint8_t read_reg[ 2 ] = { 0 };
-    uint16_t read_data = 0;
-
-    write_reg[ 0 ] = reg;
-
-	i2c_master_write( &ctx->i2c, write_reg, 1 );
-	com_delay();
-	i2c_master_read( &ctx->i2c, read_reg, 2 );
-
-    read_data = read_reg[ 0 ];
-    read_data = read_data << 8 ;
-    read_data = read_data | read_reg[ 1 ];
-    
-    return read_data;
-}
-
-float temphum11_get_temperature ( temphum11_t *ctx, uint8_t temp_in )
-{
-    uint16_t temp_out = 0;
-    float temperature;
-    
-    temp_out = temphum11_read_data( ctx, TEMPHUM11_REG_TEMPERATURE );
-    temperature = ( float )( temp_out / 65536.0 ) * 165.0 - 40;
-
-    conv_delay( );
-    
-    if ( temp_in == TEMPHUM11_TEMP_IN_KELVIN )
-    {
-        temperature = temperature + 273.15;
-    }
-    else if ( temp_in == TEMPHUM11_TEMP_IN_FAHRENHEIT )
-    {
-        temperature = ( temperature * 9.0/5.0 ) + 32;
-    }
-    
-    return temperature;
-}
-
-float temphum11_get_humidity ( temphum11_t *ctx )
-{
-    uint16_t hum_out = 0;
-    float humidity;
-
-    hum_out = temphum11_read_data( ctx, TEMPHUM11_REG_HUMIDITY );
-    humidity = ( float )( hum_out / 65536.0 ) * 100.0;
-    
-    conv_delay( );
-    
-    return humidity;
-}
-
-// ----------------------------------------------- PRIVATE FUNCTION DEFINITIONS
-
-static void com_delay ( )
-{
-    Delay_10ms( );
-}
-
-static void conv_delay ( )
-{
-    Delay_5ms( );
-    Delay_1ms( );
-    Delay_1ms( );
-}
-
+#endif
+#endif  // _TEMPHUM11_H_
 
 // ------------------------------------------------------------------------- END
